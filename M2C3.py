799122b9ea72a47902b637eae54b8552d5c51c1c
PaulaLuinaFernandez <152091955+PaulaLuinaFernandez@users.noreply.github.com>
2024-03-12 15:22:17 +0100

    Add files via upload

---
+++ M2C3.py
@@ -0,0 +1,37 @@
+# Ejercicio_0
+cadena = 'Hola mundo'
+posicion = cadena.find('Hola')
+palabra = cadena[posicion:posicion+4]
+cadena = cadena.replace('Hola', 'adiós')
+
+print(cadena)
+
+# Exercise_1
+sentence = 'This is the M2C3 exercise'
+number = 333
+colors = ['blue', 'yellow', 'red', 'orange', 'green', 'black']
+check = True
+
+Exercise_2 = sentence[:3]
+Exercise_3 = colors[0]
+Exercise_4 = number + 10
+Exercise_5 = colors[-1]
+
+names = 'Harry,alex,susie,jared,gail,conner'
+Exercise_6 = names.split(',')
+
+first_word = sentence[:4].upper()
+Exercise_7 = first_word + sentence[4:]
+
+Exercise_8 = f'My favorite number is {number}'
+
+print(Exercise_2)
+print(Exercise_3)
+print(Exercise_4)
+print(Exercise_5)
+print(Exercise_6)
+print(Exercise_7)
+print(Exercise_8)
+
+#Exercise_9
+print('hello world')
